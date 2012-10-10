9163313e210d2ae625d6a9eab3ba09f67b352262
Dominic Szablewski <mail@phoboslab.org>
2012-10-10 18:53:44 +0200

    Moved EJ_BIND_ENUM to the EJBindingBase header

--- Classes/Ejecta/EJCanvas/EJBindingCanvas.h
+++ Classes/Ejecta/EJCanvas/EJBindingCanvas.h
@@ -50,39 +50,6 @@ static const char * EJScalingModeNames[] = {
 	[kEJScalingModeFitHeight] = "fit-height"
 };
 
-// ------------------------------------------------------------------------------------
-// Shorthand to bind enums with name tables as defined above
-
-#define EJ_BIND_ENUM(name, enumNames, target) \
-	EJ_BIND_GET(name, ctx) { \
-		JSStringRef src = JSStringCreateWithUTF8CString( enumNames[target] ); \
-		JSValueRef ret = JSValueMakeString(ctx, src); \
-		JSStringRelease(src); \
-		return ret; \
-	} \
-	\
-	EJ_BIND_SET(name, ctx, value) { \
-		JSStringRef str = JSValueToStringCopy(ctx, value, NULL); \
-		const JSChar * strptr = JSStringGetCharactersPtr( str ); \
-		int length = JSStringGetLength(str)-1; \
-		for( int i = 0; i < sizeof(enumNames)/sizeof(enumNames[0]); i++ ) { \
-			if( JSStrIsEqualToStr( strptr, enumNames[i], length) ) { \
-				target = i; \
-				break; \
-			} \
-		} \
-		JSStringRelease( str );\
-	}
-
-static inline bool JSStrIsEqualToStr( const JSChar * s1, const char * s2, int length ) {
-	for( int i = 0; i < length && *s1 != '\0' && *s1 == *s2; i++ ) {
-		s1++;
-		s2++;
-	}
-	return (*s1 == *s2);
-}
-
-
 
 @interface EJBindingCanvas : EJBindingBase <EJDrawable> {
 	EJCanvasContext * renderingContext;
