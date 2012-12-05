1ad74d12ad597fd47515bf2c372f9fe5a748d313
Dominic Szablewski <mail@phoboslab.org>
2012-12-05 16:05:27 +0100

    Removed intermediate methods for get/set on* events

--- Source/Ejecta/EJBindingEventedBase.h
+++ Source/Ejecta/EJBindingEventedBase.h
@@ -5,13 +5,30 @@
 // Events; shorthand for EJ_BIND_GET/SET - use with EJ_BIND_EVENT( eventname );
 
 #define EJ_BIND_EVENT(NAME) \
-	EJ_BIND_GET(on##NAME, ctx ) { \
-		return [self getCallbackWith:( @ #NAME) ctx:ctx]; \
+	static JSValueRef _get_on##NAME( \
+		JSContextRef ctx, \
+		JSObjectRef object, \
+		JSStringRef propertyName, \
+		JSValueRef* exception \
+	) { \
+		id instance = (id)JSObjectGetPrivate(object); \
+		return (JSValueRef)objc_msgSend(instance, @selector(getCallbackWith:ctx:), ( @ #NAME), ctx); \
 	} \
-	EJ_BIND_SET(on##NAME, ctx, callback) {\
-		[self setCallbackWith:( @ #NAME) ctx:ctx callback:callback]; \
-	}
-
+	__EJ_GET_POINTER_TO(_get_on##NAME) \
+	\
+	static bool _set_on##NAME( \
+		JSContextRef ctx, \
+		JSObjectRef object, \
+		JSStringRef propertyName, \
+		JSValueRef value, \
+		JSValueRef* exception \
+	) { \
+		id instance = (id)JSObjectGetPrivate(object); \
+		objc_msgSend(instance, @selector(setCallbackWith:ctx:callback:), ( @ #NAME), ctx, value); \
+		return true; \
+	} \
+	__EJ_GET_POINTER_TO(_set_on##NAME)
+	
 @interface EJBindingEventedBase : EJBindingBase {
 	NSMutableDictionary * eventListeners; // for addEventListener
 	NSMutableDictionary * onCallbacks; // for on* setters
