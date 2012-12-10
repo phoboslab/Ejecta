2207be2f2d3aa27538f82dae0a829135eb296d84
Dominic Szablewski <mail@phoboslab.org>
2012-12-11 00:17:11 +0100

    Removed old Array conversion functions

--- Source/Ejecta/EJConvert.h
+++ Source/Ejecta/EJConvert.h
@@ -15,9 +15,6 @@ double JSValueToNumberFast( JSContextRef ctx, JSValueRef v );
 EJColorRGBA JSValueToColorRGBA(JSContextRef ctx, JSValueRef value);
 JSValueRef ColorRGBAToJSValue( JSContextRef ctx, EJColorRGBA c );
 
-JSObjectRef ByteArrayToJSObject( JSContextRef ctx, unsigned char * bytes, int count );
-void JSObjectToByteArray( JSContextRef ctx, JSObjectRef array, unsigned char * bytes, int count );
-
 #ifdef __cplusplus
 }
 #endif
