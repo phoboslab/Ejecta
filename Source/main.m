62201685b8170efc85458ffdb7251f86acbdb5da
Dominic Szablewski <mail@phoboslab.org>
2013-01-05 22:21:05 +0100

    Fixed #87 - enabled support for denormal numbers

--- Source/main.m
+++ Source/main.m
@@ -1,6 +1,21 @@
+#import <fenv.h>
+#import <TargetConditionals.h>
 #import <UIKit/UIKit.h>
 
+#if !TARGET_IPHONE_SIMULATOR
+#import <arm/arch.h>
+#endif
+
 int main(int argc, char *argv[]) {
+	#ifdef _ARM_ARCH_7
+		// Enable IEEE-754 denormal support. Needed for JavaScript's MIN_VALUE
+		// and floating point arithmetic with small numbers
+		fenv_t env;
+		fegetenv(&env);
+		env.__fpscr &= ~__fpscr_flush_to_zero;
+		fesetenv(&env);
+	#endif
+	
 	@autoreleasepool {
 		return UIApplicationMain(argc, argv, nil, nil);
 	}
