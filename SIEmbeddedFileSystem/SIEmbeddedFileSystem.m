f66ddda11827f0db29ef4773def20272fb10340a
Shaun Inman <shauninman@gmail.com>
2013-06-01 18:53:43 -0600

    added SIEmbeddedFileSystem to protect JavaScript source
    Programs writing programs! During the build phase embed.php uses xxd to
    generate C code for every JavaScript file in the project. This code is
    used by the SIEmbeddedFileSystem to access the JavaScript source (via
    the original path) without exposing it in an unarchived IPA. copy.php
    copies everything but the JavaScript files to the new IPA.

---
+++ SIEmbeddedFileSystem/SIEmbeddedFileSystem.m
@@ -0,0 +1,46 @@
+//
+//  SIEmbeddedFileSystem.m
+//  SIEmbeddedFileSystem
+//
+//  Created by Shaun Inman on 6/1/13.
+//  Copyright (c) 2013 Shaun Inman. All rights reserved.
+//
+
+#import "SIEmbeddedFileSystem.h"
+#import "SIEmbeddedFiles.h"
+
+@implementation SIEmbeddedFileSystem
+
+-(id)init
+{
+	if (self = [super init])
+	{
+		files = [[NSMutableDictionary alloc] init];
+		NSData *data;
+		NSString *jsString;
+
+		// SILENCE WARNINGS UNTIL BUILD SCRIPT IS RUN
+		data = nil;
+		jsString = nil;
+		
+		// BUILD PHASE SCRIPT WILL WORK MAGIC HERE
+	}
+	return self;
+}
+
++(SIEmbeddedFileSystem *)sharedInstance
+{
+    static SIEmbeddedFileSystem *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[SIEmbeddedFileSystem alloc] init];
+    });
+    return sharedInstance;
+}
+
+-(NSString *)stringWithContentsOfFile:(NSString *)path
+{
+	return [files objectForKey:path];
+}
+
+@end
