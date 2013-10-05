d8d5da4d711ea1f79ef7b04899a778b3a390308e
Dominic Szablewski <mail@phoboslab.org>
2013-10-05 16:38:51 +0200

    Provide public prototype for logException; see #225

--- Source/Ejecta/EJJavaScriptView.h
+++ Source/Ejecta/EJJavaScriptView.h
@@ -94,6 +94,7 @@
 - (void)loadScriptAtPath:(NSString *)path;
 - (JSValueRef)evaluateScript:(NSString *)script;
 - (JSValueRef)evaluateScript:(NSString *)script sourceURL:(NSString *)sourceURL;
+- (void)logException:(JSValueRef)exception ctx:(JSContextRef)ctx;
 - (JSValueRef)jsValueForPath:(NSString *)objectPath;
 
 - (void)clearCaches;
