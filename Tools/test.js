0e3251e8f05e97f19d8bcb8efadef52da58cc00a
finscn <finscn@gmail.com>
2013-10-09 21:42:32 +0800

    add JS File Encryption(Encode) Tool

---
+++ Tools/test.js
@@ -0,0 +1,18 @@
+
+// Test Encrypt JavaScript
+// 测试JS文件加密功能
+
+var w = window.innerWidth;
+var h = window.innerHeight;
+var w2 = w/2;
+var h2 = h/2;
+
+var canvas = document.getElementById('canvas');
+canvas.width = w;
+canvas.height = h;
+
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle="red";
+ctx.fillRect(50,50,205,100);
+
