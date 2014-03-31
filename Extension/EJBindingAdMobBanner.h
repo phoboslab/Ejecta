854a7539f076caffe487a1011903a7b0e95559a8
finscn <finscn@gmail.com>
2014-04-01 02:02:09 +0800

    update

--- Extension/EJBindingAdMobBanner.h
+++ Extension/EJBindingAdMobBanner.h
@@ -4,16 +4,15 @@
 @class GADBannerView;
 @class GADRequest;
 
-
 typedef enum {
-	banner,
-	mediumrectangle,
-	fullbanner,
-	leaderboard,
-	skyscraper,
-	portrait,
-	landscape,
-	invalid
+	banner,             // 320x50
+	mediumrectangle,    // 300x250
+	fullbanner,         // 468x60
+	leaderboard,        // 728x90
+	skyscraper,         // 120x600
+	portrait,           // portraitx32/50/90
+	landscape,          // landscapex32/50/90
+	invalid             // 
 } BannerType;
 
 @interface EJBindingAdMobBanner : EJBindingEventedBase <GADBannerViewDelegate>
