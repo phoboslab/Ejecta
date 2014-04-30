9be816f515e42e13e73e3e26c83b7b6fdcf2c4e8
finscn <finscn@gmail.com>
2014-04-30 22:44:59 +0800

    update

--- Extension/EJBindingAppUtils.m
+++ Extension/EJBindingAppUtils.m
@@ -1,31 +1,29 @@
-
 #import "EJBindingAppUtils.h"
 #import "OpenUDID.h"
-#import "base64.h"
+
 
 @implementation EJBindingAppUtils
 
 
-
-
-EJ_BIND_GET(udid, ctx) {
-	NSString* openUDID = [OpenUDID value];
+EJ_BIND_GET(udid, ctx)
+{
+	NSString *openUDID = [OpenUDID value];
 	return NSStringToJSValue(ctx, openUDID);
 }
 
 
-EJ_BIND_GET(uuid, ctx) {
-	NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
-	static NSString* UUID_KEY = @"EJ_APP_UUID";
+EJ_BIND_GET(uuid, ctx)
+{
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	static NSString *UUID_KEY = @"EJ_APP_UUID";
 
-	NSString* app_uuid = [userDefaults stringForKey:UUID_KEY];
+	NSString *app_uuid = [userDefaults stringForKey:UUID_KEY];
 
 	if (app_uuid == nil) {
-
 		CFUUIDRef uuidRef = CFUUIDCreate(kCFAllocatorDefault);
 		CFStringRef uuidString = CFUUIDCreateString(kCFAllocatorDefault, uuidRef);
 
-		app_uuid = [NSString stringWithString:(NSString*)uuidString];
+		app_uuid = [NSString stringWithString:(NSString *)uuidString];
 		[userDefaults setObject:app_uuid forKey:UUID_KEY];
 		[userDefaults synchronize];
 
@@ -35,31 +33,33 @@ EJ_BIND_GET(uuid, ctx) {
 	return NSStringToJSValue(ctx, app_uuid);
 }
 
-EJ_BIND_GET(ver, ctx){
-	NSString *ver=[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+EJ_BIND_GET(ver, ctx)
+{
+	NSString *ver = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
 	return NSStringToJSValue(ctx, ver);
 }
 
-EJ_BIND_GET(systemLocal, ctx) {
+EJ_BIND_GET(systemLocal, ctx)
+{
 	NSString *preferredLang = [[NSLocale preferredLanguages] objectAtIndex:0];
 	return NSStringToJSValue(ctx, preferredLang);
 }
 
-EJ_BIND_FUNCTION( eval, ctx, argc, argv ) {
-
+EJ_BIND_FUNCTION(eval, ctx, argc, argv)
+{
 	NSString *script = JSValueToNSString(ctx, argv[0]);
 
-	JSValueRef result=[scriptView evaluateScript:script];
+	JSValueRef result = [scriptView evaluateScript:script];
 
 	//  JSGlobalContextRef jsGlobalContext=[scriptView jsGlobalContext];
 	//  JSType type=JSValueGetType(jsGlobalContext,result);
 
 
 	return result;
-
 }
 
-EJ_BIND_GET(systemVersion, ctx ) {
+EJ_BIND_GET(systemVersion, ctx)
+{
 	return NSStringToJSValue(ctx, [[UIDevice currentDevice] systemVersion]);
 }
 
