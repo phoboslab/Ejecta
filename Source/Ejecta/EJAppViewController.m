ca35a56e022ba440f636a5e267cd91e9c5ca07e7
Dominic Szablewski <mail@phoboslab.org>
2014-09-27 13:49:29 +0200

    Fix rotation on iOS8 - second try; close #424

--- Source/Ejecta/EJAppViewController.m
+++ Source/Ejecta/EJAppViewController.m
@@ -26,13 +26,13 @@
 
 - (void)loadView {
 	CGRect frame = UIScreen.mainScreen.bounds;
+
+	// iOS pre 8.0 doesn't rotate the frame size in landscape mode, so we have to
+	// do it ourselfs
+	if( landscapeMode && EJECTA_SYSTEM_VERSION_LESS_THAN(@"8.0") ) {
+		frame.size = CGSizeMake(frame.size.height, frame.size.width);
+	}
 	
-	// iOS pre 8.0 doesn't rotate the frame size)
-	#if !defined(__IPHONE_8_0) || __IPHONE_OS_VERSION_MAX_ALLOWED < __IPHONE_8_0
-		if( landscapeMode ) {
-			frame.size = CGSizeMake(frame.size.height, frame.size.width);
-		}
-	#endif
 	EJJavaScriptView *view = [[EJJavaScriptView alloc] initWithFrame:frame];
 	self.view = view;
 	
