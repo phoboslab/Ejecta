c57f5779018051b510a7bd13ffa4a2d3512954ba
Rocky Wei <finscn@gmail.com>
2015-04-13 14:02:32 +0800

    Add a currentTransform getter on EJBindingCanvasContext2D

--- Source/Ejecta/EJCanvas/2D/EJBindingCanvasContext2D.m
+++ Source/Ejecta/EJCanvas/2D/EJBindingCanvasContext2D.m
@@ -262,6 +262,18 @@ EJ_BIND_FUNCTION(setTransform, ctx, argc, argv) {
 	return NULL;
 }
 
+EJ_BIND_GET(currentTransform, ctx) {
+    
+    NSDictionary *transform =  @{@"a": @(renderingContext.state->transform.a),
+                                 @"b": @(renderingContext.state->transform.b),
+                                 @"c": @(renderingContext.state->transform.c),
+                                 @"d": @(renderingContext.state->transform.d),
+                                 @"e": @(renderingContext.state->transform.tx),
+                                 @"f": @(renderingContext.state->transform.ty),
+                                 };
+    return NSObjectToJSValue(ctx, transform);
+}
+
 EJ_BIND_FUNCTION(drawImage, ctx, argc, argv) {
 	if( argc < 3 ) { return NULL; }
 	
