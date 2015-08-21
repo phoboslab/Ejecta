f964ecfc8e2526927fe27ff02c594b5c460a1dca
Rocky Wei <finscn@gmail.com>
2015-08-21 20:55:29 +0800

    add feature: download file

--- Extension/EJBindingAppUtils.m
+++ Extension/EJBindingAppUtils.m
@@ -5,6 +5,60 @@
 @implementation EJBindingAppUtils
 
 
+- (void) download:(NSString *)urlStr destination:(NSString *)destination callback:(JSObjectRef)callback {
+    
+    NSString *filePath = [scriptView pathForResource:destination];
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlStr]];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+        
+        NSString *errorDesc = NULL;
+        if (error) {
+            NSLog(@"Download Error:%@",error.description);
+        }
+        if (data) {
+            [data writeToFile:filePath atomically:YES];
+            NSLog(@"File is saved to %@",filePath);
+        }
+        
+        if( callback ) {
+            JSContextRef gctx = scriptView.jsGlobalContext;
+            JSStringRef jsFilePath = JSStringCreateWithUTF8CString([filePath UTF8String]);
+            JSStringRef jsErrorDesc = errorDesc?JSStringCreateWithUTF8CString([errorDesc UTF8String]):NULL;
+            
+            JSValueRef params[] = {
+                error?JSValueMakeString(gctx, jsErrorDesc):JSValueMakeNull(gctx),
+                JSValueMakeString(gctx, jsFilePath)
+            };
+            [scriptView invokeCallback:callback thisObject:NULL argc:2 argv:params];
+            JSValueUnprotectSafe(gctx, callback);
+        }
+    }];
+    
+}
+
+
+EJ_BIND_FUNCTION(download, ctx, argc, argv)
+{
+
+    NSString *url = JSValueToNSString(ctx, argv[0]);
+    NSString *destination = JSValueToNSString(ctx, argv[1]);
+    JSObjectRef callback = nil;
+    
+    if (argc == 3){
+        callback = JSValueToObject(ctx, argv[2], NULL);
+        if (callback) {
+            JSValueProtect(ctx, callback);
+        }
+    }
+
+    [self download:url destination:destination callback:callback];
+                                                         
+    return JSValueMakeBoolean(ctx, true);
+}
+
+
 EJ_BIND_GET(udid, ctx)
 {
 	NSString *openUDID = [OpenUDID value];
@@ -62,6 +116,7 @@ EJ_BIND_GET(systemVersion, ctx)
 {
 	return NSStringToJSValue(ctx, [[UIDevice currentDevice] systemVersion]);
 }
+
 
 
 @end
