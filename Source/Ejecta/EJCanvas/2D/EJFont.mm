dd05e158126813a13a6ca7322e0e9e6ecbec7495
Dominic Szablewski <mail@phoboslab.org>
2015-11-03 16:55:29 +0100

    Fix #564 - used wrong font for drawing individual runs

--- Source/Ejecta/EJCanvas/2D/EJFont.mm
+++ Source/Ejecta/EJCanvas/2D/EJFont.mm
@@ -245,7 +245,7 @@ int EJFontGlyphLayoutSortByTextureIndex(const void *a, const void *b) {
 	
 	// Render glyph and update the texture
 	CGPoint p = CGPointMake(-glyphInfo->x, -glyphInfo->y);
-	CTFontDrawGlyphs(ctMainFont, &glyph, &p, 1, context);
+	CTFontDrawGlyphs(font, &glyph, &p, 1, context);
 	[texture updateWithPixels:pixels atX:txLineX y:txLineY width:pxWidth height:pxHeight];
 	
 	// Update texture coordinates
