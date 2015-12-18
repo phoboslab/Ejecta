3270fc00dbad44016025e4972357a7ba3db8a1d6
Dominic Szablewski <mail@phoboslab.org>
2015-12-18 16:28:05 +0100

    Use retina res in example .js

--- index.js
+++ index.js
@@ -6,14 +6,18 @@
 */
 var w = window.innerWidth;
 var h = window.innerHeight;
+var scale = window.devicePixelRatio;
 var w2 = w/2;
 var h2 = h/2;
 
 var canvas = document.getElementById('canvas');
-canvas.width = w;
-canvas.height = h;
+canvas.width = w * scale;
+canvas.height = h * scale;
+canvas.style.width = w;
+canvas.style.height = h;
 
 var ctx = canvas.getContext('2d');
+ctx.scale(scale, scale);
 
 
 var curves = [];
