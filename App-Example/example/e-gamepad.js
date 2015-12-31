f896e3feb5c303c1be21b62298f1d216f73ae35d
Rocky Wei <finscn@gmail.com>
2016-01-01 01:04:05 +0800

    update examples

--- App-Example/example/e-gamepad.js
+++ App-Example/example/e-gamepad.js
@@ -10,8 +10,7 @@ if (canvas.width < canvas.height) {
 
 context.textAlign = 'left';
 context.font = '32px Helvetica';
-context.fillStyle = '#fff';
-context.strokeStyle = '#fff';
+context.strokeStyle = '#222222';
 context.lineWidth = 2;
 
 console.log("canvas size : ", canvas.width, canvas.height)
@@ -23,9 +22,9 @@ console.log("canvas size : ", canvas.width, canvas.height)
 var drawButton = function(button, x, y) {
     context.fillRect(x - 24, y + 24 - 48 * button.value, 48, 48 * button.value);
 
-    context.strokeStyle = button.pressed ? '#0f0' : '#ccc';
+    context.strokeStyle = button.pressed ?'#a30' : '#222';
     context.strokeRect(x - 24, y - 24, 48, 48);
-    context.strokeStyle = '#fff';
+    context.strokeStyle = '#222';
 };
 
 var drawAnalogStick = function(axisX, axisY, x, y) {
@@ -39,26 +38,10 @@ var drawAnalogStick = function(axisX, axisY, x, y) {
 };
 
 setInterval(function() {
-    // context.clearRect(0, 0, width / scale + 2, height / scale + 2);
-    context.clearRect(0, 0, width, height);
+    context.fillStyle = '#ffffff';
+    context.fillRect(0, 0, width, height);
 
-    // Always use 2nd gamepad for this visualization if it's present. On an
-    // Apple TV when the TV Remote and a Game Controller is connected, the Remote
-    // will be the first entry in the gamepads array; we want a dedicated
-    // Game Controller if there is one.
-
-    var gamepads = navigator.getGamepads();
-    var gamepad = gamepads[1] || gamepads[0];
-
-    if (!gamepad) {
-        context.fillText('No Gamepads connected', 32, 32);
-        // console.log('No Gamepads connected');
-        return;
-    }
-
-    gamepad.allowsRotation = true;
-
-    context.fillText('Using Gamepad: #' + gamepad.index + ' (' + gamepad.id + ')', 32, 32);
+    context.fillStyle = '#222222';
 
     var tX = 32,
         tY = canvas.height - 240;
@@ -89,46 +72,55 @@ setInterval(function() {
     // console.log(JSON.stringify(gamepad.motion));
     // console.log('Using Gamepad: #' + gamepad.index + ' (' + gamepad.id + ')');
 
-    context.translate(210, 210);
+
+    var gamepads = navigator.getGamepads();
+
+    context.save();
+    handleGamepad(gamepads[0], context, 30, 50);
+    context.restore();
+    context.save();
+    handleGamepad(gamepads[1], context, canvas.width / 2 + 30, canvas.height * 1 / 3 + 50);
+    context.restore();
+
+
+}, 16);
+
+function handleGamepad(gamepad, context, x, y) {
+    context.translate(x, y);
+
+    if (!gamepad) {
+        context.fillText('No Gamepads connected', 32, 32);
+        // console.log('No Gamepads connected');
+        // context.translate(-x, -y);
+        return;
+    }
+
+    gamepad.allowsRotation = true;
+    gamepad.exitOnMenuPress = false;
+    context.fillText('Using Gamepad: #' + gamepad.index + ' (' + gamepad.id + ')', 32, 32);
+
 
     // Button Mappings according to http://www.w3.org/TR/gamepad/#remapping
 
-    drawButton(gamepad.buttons[0], 928, 354); // A
-    drawButton(gamepad.buttons[1], 994, 288); // B
-    drawButton(gamepad.buttons[2], 862, 288); // X
-    drawButton(gamepad.buttons[3], 928, 224); // Y
+    drawButton(gamepad.buttons[0], 728, 354); // A
+    drawButton(gamepad.buttons[1], 794, 288); // B
+    drawButton(gamepad.buttons[2], 662, 288); // X
+    drawButton(gamepad.buttons[3], 728, 224); // Y
 
-    drawButton(gamepad.buttons[4], 412, 96); // L1
-    drawButton(gamepad.buttons[5], 868, 96); // R1
-    drawButton(gamepad.buttons[6], 288, 96); // L2
-    drawButton(gamepad.buttons[7], 994, 96); // R2
+    drawButton(gamepad.buttons[4], 212, 96); // L1
+    drawButton(gamepad.buttons[5], 668, 96); // R1
+    drawButton(gamepad.buttons[6], 88, 96); // L2
+    drawButton(gamepad.buttons[7], 794, 96); // R2
 
-    drawButton(gamepad.buttons[12], 352, 224); // Up
-    drawButton(gamepad.buttons[13], 352, 354); // Down
-    drawButton(gamepad.buttons[14], 288, 288); // Left
-    drawButton(gamepad.buttons[15], 416, 288); // Right
+    drawButton(gamepad.buttons[12], 152, 224); // Up
+    drawButton(gamepad.buttons[13], 152, 354); // Down
+    drawButton(gamepad.buttons[14], 88, 288); // Left
+    drawButton(gamepad.buttons[15], 216, 288); // Right
 
-    drawButton(gamepad.buttons[16], 640, 196); // Menu
+    drawButton(gamepad.buttons[16], 440, 196); // Menu
 
-    drawAnalogStick(gamepad.axes[0], gamepad.axes[1], 540, 416); // left stick
-    drawAnalogStick(gamepad.axes[2], gamepad.axes[3], 736, 416); // right stick
+    drawAnalogStick(gamepad.axes[0], gamepad.axes[1], 340, 416); // left stick
+    drawAnalogStick(gamepad.axes[2], gamepad.axes[3], 536, 416); // right stick
 
-    context.translate(-210, -210);
-
-
-    // You can control whether the MENU button exits your game. Apple will reject your
-    // App if it does not. For Gamepads, the B button can also act as a MENU button.
-    // However, MENU should only exit the App the in certain cases.
-
-    // The expected behavior is this:
-    // > Pauses/resumes gameplay. Returns to previous screen, exits to main game menu,
-    // > and/or exits to Apple TV Home screen.
-    // ~ https://developer.apple.com/tvos/human-interface-guidelines/remote-and-interaction/
-
-    // In any case, judging from other games, you're probably safe to set 'exitOnMenuPress'
-    // to false during gameplay and only set it to true when you're on the Title Screen of
-    // your game.
-
-    gamepad.exitOnMenuPress = !true;
-
-}, 16);
+    // context.translate(-x, -y);
+}
