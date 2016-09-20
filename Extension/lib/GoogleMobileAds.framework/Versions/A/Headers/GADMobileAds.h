fd53c683c4703f7aa519a972b667d2c3e06d392f
Rocky Wei <finscn@gmail.com>
2016-09-21 05:13:16 +0800

    update admobSDK

--- Extension/lib/GoogleMobileAds.framework/Versions/A/Headers/GADMobileAds.h
+++ Extension/lib/GoogleMobileAds.framework/Versions/A/Headers/GADMobileAds.h
@@ -36,4 +36,13 @@
 /// muting). Defaults to NO.
 @property(nonatomic, assign) BOOL applicationMuted;
 
+/// Returns YES if the current SDK version is at least |major|.|minor|.|patch|. This method can be
+/// used by libraries that depend on a specific minimum version of the Google Mobile Ads SDK to warn
+/// developers if they have an incompatible version.
+///
+/// Available in Google Mobile Ads SDK 7.10 and onwards. Before calling this method check if the
+/// GADMobileAds's shared instance responds to this method. Calling this method on a Google Mobile
+/// Ads SDK lower than 7.10 can crash the app.
+- (BOOL)isSDKVersionAtLeastMajor:(NSInteger)major minor:(NSInteger)minor patch:(NSInteger)patch;
+
 @end
