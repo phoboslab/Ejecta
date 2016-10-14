53834420047dd6e11f052f5493a57c17a838681d
Rocky Wei <finscn@gmail.com>
2016-10-15 07:16:13 +0800

    update test for instanceof

--- App-Example/example/e-test.js
+++ App-Example/example/e-test.js
@@ -1,14 +1,17 @@
 console.log("This is e-test.js");
 
+(function() {
 
-var img = new Image();
-console.log("HTMLImageElement: ", img instanceof HTMLImageElement);
+    var img = new Image();
+    console.log("HTMLImageElement: ", img instanceof HTMLImageElement);
 
-var canvas = document.createElement("canvas");
-console.log("HTMLCanvasElement: ", canvas instanceof HTMLCanvasElement);
+    var canvas = document.createElement("canvas");
+    console.log("HTMLCanvasElement: ", canvas instanceof HTMLCanvasElement);
 
-var video = new Video();
-console.log("HTMLVideoElement: ", video instanceof HTMLVideoElement);
+    var video = new Video();
+    console.log("HTMLVideoElement: ", video instanceof HTMLVideoElement);
 
-var audio = new Audio();
-console.log("HTMLAudioElement: ", audio instanceof HTMLAudioElement);
+    var audio = new Audio();
+    console.log("HTMLAudioElement: ", audio instanceof HTMLAudioElement);
+
+})();
