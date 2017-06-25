399a10f446a878baa95d5cffad6ec2d602acf71a
Dominic Szablewski <mail@phoboslab.org>
2017-06-25 15:15:01 +0200

    Disable Image Picker by default; close #655

--- Source/Ejecta/EJUtils/EJBindingImagePicker.h
+++ Source/Ejecta/EJUtils/EJBindingImagePicker.h
@@ -5,6 +5,23 @@
 // The image is returned as EJBindingImage instance to JavaScript and can be
 // directly drawn onto a Canvas or loaded as a WebGL texture.
 
+// The Image Picker is disabled by default, because Apple will reject your
+// app from the appstore if this functionality is compiled in, but an
+// `NSPhotoLibraryUsageDescription` key is missing from the app's plist,
+// explaining what it's used for.
+
+
+
+// To enable the image picker, change the following `EJ_PICKER_ENABLED` to
+// true and add the `NSPhotoLibraryUsageDescription` key to your info.plist.
+
+#define EJ_PICKER_ENABLED false
+
+
+
+
+#if EJ_PICKER_ENABLED
+
 #import "EJBindingBase.h"
 
 #define EJ_PICKER_TYPE_FULLSCREEN 1
@@ -35,3 +52,5 @@ typedef enum {
 + (BOOL)isSourceTypeAvailable:(NSString *) sourceType;
 
 @end
+
+#endif
